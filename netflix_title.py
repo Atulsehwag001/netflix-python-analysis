bf83f9735a849ae89d2a4f53472550d4d688db9c
Atulsehwag001 <s3212716@gmail.com>
2025-11-02 21:08:43 +0530

    Add files via upload

---
+++ netflix_title.py
@@ -0,0 +1,15 @@
+# Netflix Top 10 Countries
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("netflix_titles.csv")
+top_countries = df['country'].value_counts().head(10)
+
+plt.figure(figsize=(8,6))
+top_countries.plot(kind='bar', color='red')
+plt.title("Top 10 Countries by Netflix Content", fontsize=16)
+plt.xlabel("Country")
+plt.ylabel("Number of Titles")
+plt.xticks(rotation=45)
+plt.savefig("netflix_top_countries.png")
+plt.show()
